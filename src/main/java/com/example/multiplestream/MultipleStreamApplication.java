feb85154c44b2413240fa00b4d14bdd1080d6641
sbagrec1 <shubhamcs@optum.com>
2021-03-03 21:34:45 +0530

    add uncaught execption handler

--- src/main/java/com/example/multiplestream/MultipleStreamApplication.java
+++ src/main/java/com/example/multiplestream/MultipleStreamApplication.java
@@ -6,6 +6,7 @@ import org.apache.kafka.streams.kstream.ValueMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.kafka.config.StreamsBuilderFactoryBeanCustomizer;
 
 import java.util.function.Function;
 
@@ -19,6 +20,19 @@ public class MultipleStreamApplication {
                 input.peek((key, value) -> log.info("Received key:{}, value:{}", key, value))
                         .mapValues((ValueMapper<String, String>) String::toUpperCase)
                         .peek((key, value) -> log.info("Output key:{}, value:{}", key, value));
+    }
+
+    @Bean
+    public StreamsBuilderFactoryBeanCustomizer streamsBuilderFactoryBeanCustomizer() {
+        return factoryBean ->
+                factoryBean.setKafkaStreamsCustomizer(
+                        kafkaStreams -> {
+                            kafkaStreams.setUncaughtExceptionHandler(
+                                    (t, e) -> {
+                                        log.error("Error occurred while Processing, shutting down: ", e);
+                                        System.exit(0);
+                                    });
+                        });
     }
 
     public static void main(String[] args) {
