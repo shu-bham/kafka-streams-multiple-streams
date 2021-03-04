5c0f277a28c8ffd2554d511b8a2b9923279e71bf
sbagrec1 <shubhamcs@optum.com>
2021-03-04 17:20:29 +0530

    add branching

--- src/main/java/com/example/multiplestream/MultipleStreamApplication.java
+++ src/main/java/com/example/multiplestream/MultipleStreamApplication.java
@@ -1,25 +1,30 @@
 package com.example.multiplestream;
 
 import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KStream;
-import org.apache.kafka.streams.kstream.ValueMapper;
+import org.apache.kafka.streams.kstream.Predicate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.config.StreamsBuilderFactoryBeanCustomizer;
 
-import java.util.function.Function;
+import java.util.function.BiFunction;
 
 @Slf4j
 @SpringBootApplication
 public class MultipleStreamApplication {
 
     @Bean
-    public Function<KStream<String, String>, KStream<String, String>> process() {
-        return input ->
-                input.peek((key, value) -> log.info("Received key:{}, value:{}", key, value))
-                        .mapValues((ValueMapper<String, String>) String::toUpperCase)
-                        .peek((key, value) -> log.info("Output key:{}, value:{}", key, value));
+    public BiFunction<KStream<String, String>, KStream<String, String>, KStream<String, String>[]> process() {
+        Predicate<String, String> toOutputTopic1 = (k, v) -> k.equalsIgnoreCase("app1");
+        Predicate<String, String> toOutputTopic2 = (k, v) -> k.equalsIgnoreCase("app2");
+        return (input1, input2) ->
+                input1.merge(input2)
+                        .peek((key, value) -> log.info("Received key:{}, value:{}", key, value))
+                        .map((key, value) -> new KeyValue<>(key.toUpperCase(), "processed value:" + value))
+                        .peek((key, value) -> log.info("Output key:{}, value:{}", key, value))
+                        .branch(toOutputTopic1, toOutputTopic2);
     }
 
     @Bean
