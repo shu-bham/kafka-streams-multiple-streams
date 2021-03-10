d59d87ebb7acec39691166e1062320473e0ccb7b
sbagrec1 <shubhamcs@optum.com>
2021-03-10 16:44:30 +0530

    cleanup

--- src/main/java/com/example/multiplestream/MultipleStreamApplication.java
+++ src/main/java/com/example/multiplestream/MultipleStreamApplication.java
@@ -1,8 +1,8 @@
 package com.example.multiplestream;
 
 import lombok.extern.slf4j.Slf4j;
-import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.ValueMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
@@ -18,7 +18,7 @@ public class MultipleStreamApplication {
     public Function<KStream<String, String>, KStream<String, String>> processXXX() {
         return input ->
                 input.peek((key, value) -> log.info("Received-XXX: key:{}, value:{}", key, value))
-                        .map((key, value) -> new KeyValue<>(key.toUpperCase(), value.toUpperCase()))
+                        .mapValues((ValueMapper<String, String>) String::toUpperCase)
                         .peek((key, value) -> log.info("Output-XXX: key:{}, value:{}", key, value));
     }
 
@@ -26,7 +26,7 @@ public class MultipleStreamApplication {
     public Function<KStream<String, String>, KStream<String, String>> processYYY() {
         return input ->
                 input.peek((key, value) -> log.info("Received-YYY key:{}, value:{}", key, value))
-                        .map((key, value) -> new KeyValue<>(key.toUpperCase(), value.toUpperCase()))
+                        .mapValues((ValueMapper<String, String>) String::toUpperCase)
                         .peek((key, value) -> log.info("Output-YYY key:{}, value:{}", key, value));
     }
 
@@ -34,7 +34,7 @@ public class MultipleStreamApplication {
     public Function<KStream<String, String>, KStream<String, String>> processZZZ() {
         return input ->
                 input.peek((key, value) -> log.info("Received-ZZZ key:{}, value:{}", key, value))
-                        .map((key, value) -> new KeyValue<>(key.toUpperCase(), value.toUpperCase()))
+                        .mapValues((ValueMapper<String, String>) String::toUpperCase)
                         .peek((key, value) -> log.info("Output-ZZZ key:{}, value:{}", key, value));
     }
 
